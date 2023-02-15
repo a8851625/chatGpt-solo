01509bfd4cab0f8b77f70cd1fdab6edeb2a25d40
a8851625 <ca3065910>
2023-02-16 03:09:32 +0800

    fix

--- src/main/java/com/tencent/wxcloudrun/controller/ChatController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/ChatController.java
@@ -21,9 +21,6 @@ public class ChatController {
      */
     @PostMapping(value = "/api/chat")
     public void msgProcess(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        // 将请求、响应的编码均设置为UTF-8（防止中文乱码）
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
         // 调用核心业务类接收消息、处理消息
         String respMessage = processRequest(request);
         // 响应消息
